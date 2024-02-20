5f21370f4cefb6e1fcbbd240ce795f1c4443dd8f
Adolfo Machado <adolfo.machado@viajarbarato.com.br>
2024-02-20 14:41:53 -0300

    Final aula 1

---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using System.Collections.Generic;
+
+namespace RocketseatAuction.API.Repositories
+{
+    public class RocketseatAuctionDbContext : DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=C:\Projetos\RocketSeat\RocketseatAuction\leilaoDbNLW.db");
+
+        }
+    }
+}
---
+++ src/RocketseatAuction.API/UseCases/Auction/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+using System;
+using System.Linq;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent
+{
+    public class GetCurrentAuctionUseCase
+    {
+        public Auction? GetCurrent()
+        {
+            var repository = new RocketseatAuctionDbContext();
+
+            var currentDate = DateTime.Now;
+
+            var currentAuction = repository
+                .Auctions
+                .Include(auction => auction.Items)
+                .FirstOrDefault(a => currentDate >= a.Starts && currentDate <= a.Ends);
+
+            return currentAuction;
+        }
+
+        public IEnumerable<Auction> GetAll()
+        {
+            var repository = new RocketseatAuctionDbContext();
+
+            return repository
+                .Auctions
+                .Include(auction => auction.Items)
+                .ToList();
+        }
+    }
+}
